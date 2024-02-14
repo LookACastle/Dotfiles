994fa90b83f67ec7a78143cbf3dbdc10db4e0cdf
LookACastle <thorkik@gmail.com>
2024-02-14 10:50:09 +0100

    Remove ugly background colour in vue

--- doom.d/package-configs/vue-mode.el
+++ doom.d/package-configs/vue-mode.el
@@ -1,3 +1,7 @@
 ;;; $DOOMDIR/package-configs/vue-mode.el -*- lexical-binding: t; -*-
 
 (add-hook 'vue-mode-hook #'lsp!)
+
+(add-hook 'mmm-mode-hook
+          (lambda ()
+            (set-face-background 'mmm-default-submode-face nil)))
