08b3714683d99cc937c65845489859476a11de5d
LookACastle <thorkik@gmail.com>
2024-08-09 11:57:03 +0200

    reenable black

--- doom.d/init.el
+++ doom.d/init.el
@@ -56,8 +56,7 @@
        (evil +everywhere)  ; come to the dark side, we have cookies
        file-templates      ; auto-snippets for empty files
        fold                ; (nigh) universal code folding
-       (format +format-on-save-enabled-modes ; automated prettiness
-               '(not python-mode))    ; black is overzealous
+       (format +format-on-save-enabled-modes) ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
